207315b8ed5ca017588169e0954df81c50f1d136
jonhory <318075441@qq.com>
2017-03-18 12:46:43 +0800

    update

--- ASDKTextDemo/ViewController.swift
+++ ASDKTextDemo/ViewController.swift
@@ -11,33 +11,48 @@ import AsyncDisplayKit
 
 class ViewController: UIViewController {
 
-    let text1 = "ASTextNode А/В Do any additional setup after loading the view"
-    let text2 = "UILabel А/В Do any additional setup after loading the view"
+    let text1 = "TEXT АВ Do any additional setup after loading the view"
+    let text2 = "TEXT / АВ Do any additional setup after loading the view"
+    let text3 = "View;ControllerAsyncDisplayKit"
+    let text4 = "ViewControllerAsyncDisplayKit"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.lightGray
         
-        loadTextNode()
-        loadUILabel()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadTextNode(y: 100, with: "This is ASTextNode")
+        loadUILabel(y: 130, with: "This is UILabel")
+        
+        loadTextNode(y: 200, with: text1)
+        loadUILabel(y: 230, with: text1)
+        
+        loadTextNode(y: 260, with: text2)
+        loadUILabel(y: 290, with: text2)
+        
+        loadTextNode(y: 320, with: text3)
+        loadUILabel(y: 350, with: text3)
+        
+        loadTextNode(y: 380, with: text4)
+        loadUILabel(y: 410, with: text4)
     }
     
-    func loadTextNode() {
+    func loadTextNode(y: CGFloat, with text: String) {
         let textNode = ASTextNode()
-        textNode.attributedText = NSAttributedString(string: text1, attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 16), NSForegroundColorAttributeName: UIColor.red])
-        textNode.frame = CGRect(x: 100, y: 100, width: 200, height: 20)
+        textNode.attributedText = NSAttributedString(string: text,
+                                                     attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 16),
+                                                                  NSForegroundColorAttributeName: UIColor.red])
+        textNode.frame = CGRect(x: 100, y: y, width: 200, height: 20)
         textNode.backgroundColor = UIColor.blue
         view.addSubnode(textNode)
     }
     
-    func loadUILabel() {
+    func loadUILabel(y: CGFloat, with text: String) {
         let label = UILabel()
-        label.text = text2
+        label.text = text
         label.textColor = UIColor.blue
         label.backgroundColor = UIColor.red
-        label.frame = CGRect(x: 100, y: 200, width: 200, height: 20)
+        label.frame = CGRect(x: 100, y: y, width: 200, height: 20)
         view.addSubview(label)
     }
 
--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 A sample project for <https://github.com/facebook/AsyncDisplayKit/issues/2885>。
 
 I just use ASDK soon, found `ASTextNode` display abnormal when  input with some string contains `/`。
+
+## UPDATE: 
+
+### PNG show the issue:
+
+* Blue backgroundColor is `ASTextNode`, Red backgroundColor is `UILabel`
+
+![](http://ww1.sinaimg.cn/large/c6a1cfeagy1fdqvs0c0qnj20cr0lhdh2.jpg)
+
+* I found this issue show when string contains whiteSpace, `/` , `;` ,maybe include more symbols.
+
+### Here is my code:
+
+* `loadASTextNode`
+
+```
+func loadTextNode(y: CGFloat, with text: String) {
+    let textNode = ASTextNode()
+    textNode.attributedText = NSAttributedString(string: text,
+                                                 attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 16),
+                                                              NSForegroundColorAttributeName: UIColor.red])
+    textNode.frame = CGRect(x: 100, y: y, width: 200, height: 20)
+    textNode.backgroundColor = UIColor.blue
+    view.addSubnode(textNode)
+}
+```
+
+* `loadUILabel`
+
+```
+func loadUILabel(y: CGFloat, with text: String) {
+    let label = UILabel()
+    label.text = text
+    label.textColor = UIColor.blue
+    label.backgroundColor = UIColor.red
+    label.frame = CGRect(x: 100, y: y, width: 200, height: 20)
+    view.addSubview(label)
+}
+```
+
+### If my code have any question, maybe somebody can help me.Thanks.
